b584d3e2b2df0b34c042d231df7f4119a51d969e
brittanypallasco2003 <nohemiespinel0@gmail.com>
2023-12-10 18:50:03 -0500

    creación de rutas

--- src/index.js
+++ src/index.js
@@ -6,6 +6,26 @@ const express=require('express')
 //crear una instancia del web server express
 const app=express()
 
-//indicarle al servidor el n de puerto que ejecutará las peticiones
+
+//creación de rutas en express
+//ruta inicial
+app.get('/',(req, res)=>{
+    res.send("Landing Page - Grupo 1")
+})
+
+app.get('/integrantes',(req, res)=>{
+    res.send('Dashboard principal')
+})
+
+app.get('/productos',(req, res)=>{
+    res.send('Catologo de productos')
+})
+
+//personalizar el mensaje para rutas no resgistras en el web server y mandar el código de respuesta 404
+app.use((req,res)=>{
+    res.status(400).send('Página NO encontrada - 404')
+})
+
+//indicarle al servidor el n de puerto en se que ejecutará las peticiones
 app.listen(3000)
 console.log('el servidor se está ejecutando en el puerto 3000')
